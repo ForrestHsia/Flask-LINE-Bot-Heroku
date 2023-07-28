0973cd5df093ff57a1c8e4dc5513f5ffa88ecf02
ForrestHsia <forresthsia@gmail.com>
2023-07-28 15:16:32 +0800

    test 1

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 @app.route("/", methods=["GET", "POST"])
 def callback():
-
     if request.method == "GET":
         return "Hello Heroku"
     if request.method == "POST":
@@ -38,3 +37,8 @@ def handle_message(event):
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
